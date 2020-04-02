8f935a748c6d8dddd45ae63ad088b4e551d8f476
heiligbasil <basilief@yahoo.com>
2020-04-02 01:03:24 -0700

    Add class which is a response when examining

---
+++ Android/app/src/main/java/com/lambdaschool/cs_build_week_2/models/Examine.kt
@@ -0,0 +1,32 @@
+package com.lambdaschool.cs_build_week_2.models
+
+class Examine {
+    var name: String? = null
+    var description: String? = null
+    var weight: Int? = null
+    var itemtype: String? = null
+    var level: Int? = null
+    var exp: Int? = null
+    var attributes: String? = null
+    var cooldown: Double? = null
+    var errors: List<Any>? = null
+    var messages: List<Any>? = null
+
+    /**
+     * Returns a string representation of the object.
+     */
+    override fun toString(): String {
+        val sb: StringBuilder = java.lang.StringBuilder()
+        sb.appendln("Name: $name")
+        sb.appendln("Description: $description")
+        sb.appendln("Weight: $weight")
+        sb.appendln("ItemType: $itemtype")
+        sb.appendln("Level: $level")
+        sb.appendln("Exp: $exp")
+        sb.appendln("Attributes: $attributes")
+        sb.appendln("Cooldown: $cooldown")
+        sb.appendln("Errors: ${errors?.joinToString()}")
+        sb.append("Messages: ${messages?.joinToString()}")
+        return sb.toString()
+    }
+}
